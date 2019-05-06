d4b954bcd81df198e4a3a209f54d7761970eb91a
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-06 00:37:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,9 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  library
+  library["get_emoticon"].each do |english, japanese|
+    
+  end
 end
 
 def get_english_meaning
